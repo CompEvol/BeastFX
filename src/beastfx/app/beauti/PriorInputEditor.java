8d56e5d36154193209ceff075d9e03b33b12dbeb
Remco Bouckaert <higgs.ml@gmail.com>
2023-04-07 06:56:04 +1200

    Fix Prior panel parametric distribution display. fixes #44

--- src/beastfx/app/beauti/PriorInputEditor.java
+++ src/beastfx/app/beauti/PriorInputEditor.java
@@ -11,21 +11,23 @@ import javafx.geometry.Dimension2D;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
-
 import beastfx.app.inputeditor.BEASTObjectDialog;
 import beastfx.app.inputeditor.BeautiDoc;
 import beastfx.app.inputeditor.BeautiSubTemplate;
 import beastfx.app.inputeditor.InputEditor;
-import beastfx.app.util.FXUtils;
 import beastfx.app.inputeditor.ListInputEditor;
+import beastfx.app.util.FXUtils;
 import beast.base.core.BEASTInterface;
 import beast.base.core.Function;
+import beast.base.core.Function.Constant;
 import beast.base.core.Input;
 import beast.base.inference.distribution.Dirichlet;
+import beast.base.inference.distribution.ParametricDistribution;
 import beast.base.inference.distribution.Prior;
 import beast.base.inference.parameter.IntegerParameter;
 import beast.base.inference.parameter.RealParameter;
@@ -46,6 +48,8 @@ public class PriorInputEditor extends InputEditor.Base {
 		return Prior.class;
 	}
 
+	private ComboBox<BeautiSubTemplate> comboBox;
+	
 	@Override
 	public void init(Input<?> input, BEASTInterface beastObject, int listItemNr, ExpandOption isExpandOption, boolean addButtons) {
         m_bAddButtons = addButtons;
@@ -65,54 +69,10 @@ public class PriorInputEditor extends InputEditor.Base {
         label.setPrefSize(size.getWidth(), size.getHeight());
         itemBox.getChildren().add(label);
 
-        List<BeautiSubTemplate> availableBEASTObjects = doc.getInputEditorFactory().getAvailableTemplates(prior.distInput, prior, null, doc);
-        if (prior.m_x.get().getDimension() == 1) {
-        	// remove Dirichlet entry
-        	for (int i = availableBEASTObjects.size() - 1; i >=0 ; i--) {
-        		if (availableBEASTObjects.get(i).getID().equals("Dirichlet")) {
-        			availableBEASTObjects.remove(i);
-        		}
-        	}
-        }
-        ComboBox<BeautiSubTemplate> comboBox = new ComboBox<BeautiSubTemplate>();
-        comboBox.getItems().addAll(availableBEASTObjects.toArray(new BeautiSubTemplate[]{}));
-        comboBox.setId(text+".distr");
+        
 
-        String id = prior.distInput.get().getID();
+        comboBox = createComboBox();	
 
-        id = id.substring(0, id.indexOf('.'));
-        for (BeautiSubTemplate template : availableBEASTObjects) {
-            if (template.classInput.get() != null && template.shortClassName.equals(id)) {
-                comboBox.setValue(template);
-            }
-        }
-        comboBox.setOnAction(e -> {
-            @SuppressWarnings("unchecked")
-			ComboBox<BeautiSubTemplate> comboBox1 = (ComboBox<BeautiSubTemplate>) e.getSource();
-
-            List<?> list = (List<?>) m_input.get();
-
-            BeautiSubTemplate template = (BeautiSubTemplate) comboBox1.getValue();
-            PartitionContext context = doc.getContextFor((BEASTInterface) list.get(itemNr));
-            Prior prior1 = (Prior) list.get(itemNr);
-            try {
-                template.createSubNet(context, prior1, prior1.distInput, true);
-            } catch (Exception e1) {
-                e1.printStackTrace();
-            }
-
-            if (prior1.distInput.get() instanceof Dirichlet) {
-            	Input<Function> alphaInput = ((Dirichlet)prior1.distInput.get()).alphaInput;
-            	Function f = alphaInput.get();
-            	if (f instanceof RealParameter) {
-            		((RealParameter)f).setDimension(prior1.m_x.get().getDimension());
-            	}
-            }
-
-            
-            sync();
-            refreshPanel();
-        });
         Pane panel = new Pane();
         panel.getChildren().add(comboBox);
         panel.setMaxSize(size.getWidth(), size.getHeight());
@@ -175,7 +135,39 @@ public class PriorInputEditor extends InputEditor.Base {
         getChildren().add(pane);
 	}
 
-    String paramToString(RealParameter p) {
+    private String getParameters() {
+    	StringBuilder b = null;
+    	ParametricDistribution distr = (ParametricDistribution) m_beastObject.getInput("distr").get();
+    	for (Input<?> input: distr.listInputs()) {
+    		Object o = input.get();
+    		if (o != null && (o instanceof RealParameter||o instanceof Constant)) {
+    			BEASTInterface p = (BEASTInterface) o;
+    			if (b == null) {
+    				b = new StringBuilder();
+    				b.append(p.getInput("value").get().toString().trim());
+    			} else {
+    				b.append(',');
+    				b.append(p.getInput("value").get().toString().trim());
+    			}
+    		} else if (o != null && o instanceof Double && !input.getName().equals("offset")) {
+    			Double p = (Double) o;
+    			if (b == null) {
+    				b = new StringBuilder();
+    				b.append(p);
+    			} else {
+    				b.append(',');
+    				b.append(p);
+    			}
+    		}
+
+    	}
+		if (b == null) {
+			return "";
+		}
+		return "[" + b.toString().replaceAll("[\\]\\[]", "") + "]";
+	}
+
+	String paramToString(RealParameter p) {
         Double lower = p.lowerValueInput.get();
         Double upper = p.upperValueInput.get();
         return "initial = " + Arrays.toString(p.valuesInput.get().toArray()) +
@@ -191,16 +183,97 @@ public class PriorInputEditor extends InputEditor.Base {
                 "," + (upper == null ? "\u221E" : upper + "") + "]";
     }
 
+    
     VBox expandBox = null;
 	public void setExpandBox(VBox expandBox) {
 		this.expandBox = expandBox;
+		this.expandBox.visibleProperty().addListener((o, oldVal, newVal) -> {
+			Pane parent = (Pane) comboBox.getParent();
+			int i = parent.getChildren().indexOf(comboBox);
+			comboBox = createComboBox();
+			parent.getChildren().set(i, comboBox);
+     });
 	}
 	
+	private ComboBox<BeautiSubTemplate> createComboBox() {
+		ComboBox<BeautiSubTemplate> comboBox = new ComboBox<>();
+
+        Prior prior = (Prior) m_beastObject;
+        String text = prior.getParameterName();
+
+        List<BeautiSubTemplate> availableBEASTObjects = doc.getInputEditorFactory().getAvailableTemplates(prior.distInput, prior, null, doc);
+        if (prior.m_x.get().getDimension() == 1) {
+        	// remove Dirichlet entry
+        	for (int i = availableBEASTObjects.size() - 1; i >=0 ; i--) {
+        		if (availableBEASTObjects.get(i).getID().equals("Dirichlet")) {
+        			availableBEASTObjects.remove(i);
+        		}
+        	}
+        }
+
+        comboBox.getItems().addAll(availableBEASTObjects);
+        comboBox.setId(text+".distr");
+        comboBox.setButtonCell(new ListCell<BeautiSubTemplate>() {
+        	@Override
+        	protected void updateItem(BeautiSubTemplate item, boolean empty) {
+        		super.updateItem(item, empty);
+        		if (!empty && item != null) {
+        			if (expandBox.isVisible()) {
+        				setText(item.toString());
+        			} else {
+        				setText(item.toString() + getParameters());
+        			}
+                } else {
+                    setText(null);
+                }
+        	}
+        });
+
+        String id = prior.distInput.get().getID();
+
+        id = id.substring(0, id.indexOf('.'));
+        for (BeautiSubTemplate template : availableBEASTObjects) {
+            if (template.classInput.get() != null && template.shortClassName.equals(id)) {
+                comboBox.setValue(template);
+            }
+        }
+        comboBox.setOnAction(e -> {
+            @SuppressWarnings("unchecked")
+			ComboBox<BeautiSubTemplate> comboBox1 = (ComboBox<BeautiSubTemplate>) e.getSource();
+
+            List<?> list = (List<?>) m_input.get();
+
+            BeautiSubTemplate template = (BeautiSubTemplate) comboBox1.getValue();
+            PartitionContext context = doc.getContextFor((BEASTInterface) list.get(itemNr));
+            Prior prior1 = (Prior) list.get(itemNr);
+            try {
+                template.createSubNet(context, prior1, prior1.distInput, true);
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+
+            if (prior1.distInput.get() instanceof Dirichlet) {
+            	Input<Function> alphaInput = ((Dirichlet)prior1.distInput.get()).alphaInput;
+            	Function f = alphaInput.get();
+            	if (f instanceof RealParameter) {
+            		((RealParameter)f).setDimension(prior1.m_x.get().getDimension());
+            	}
+            }
+
+            
+            sync();
+            refreshPanel();
+        });
+        
+        return comboBox;
+
+	}
+
 	@Override
 	public void refreshPanel() {
 		if (expandBox != null) {
 			ListInputEditor.updateExpandBox(doc, expandBox, m_beastObject, this);
 		}
 		super.refreshPanel();
-	}	
+	}    
 }
