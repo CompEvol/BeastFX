14a571f113fb776169c82fd5fbb3b377a9b7241c
Remco Bouckaert <higgs.ml@gmail.com>
2023-08-08 06:47:06 +1200

    fix issue with warning label colouring. fixes #70

--- src/beastfx/app/inputeditor/SmallLabel.java
+++ src/beastfx/app/inputeditor/SmallLabel.java
@@ -4,8 +4,6 @@ package beastfx.app.inputeditor;
 
 import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
-import javafx.scene.layout.Background;
-import javafx.scene.paint.Paint;
 
 /**
  * Miniature round label
@@ -13,24 +11,25 @@ import javafx.scene.paint.Paint;
 public class SmallLabel extends Label {
     public String circleColor = "blue";
 
+    static private String style = 
+            "-fx-background-radius: 20em; " +
+            "-fx-min-width: 15px; " +
+            "-fx-min-height: 15px; " +
+            "-fx-max-width: 15px; " +
+            "-fx-max-height: 15px; " +
+            "-fx-background-color: ";
+    
     public SmallLabel(String label, String circleColor) {
         super(label);
         setText("");
-        String style = 
-                "-fx-background-radius: 20em; " +
-                "-fx-min-width: 15px; " +
-                "-fx-min-height: 15px; " +
-                "-fx-max-width: 15px; " +
-                "-fx-max-height: 15px; " +
-                "-fx-background-color: " + circleColor;
-        setStyle(style);
+        setStyle(style + circleColor);
 
         this.circleColor = circleColor;
         super.setVisible(true);
     } // c'tor
 
     public void setColor(String circleColor) {
-        Background.fill(Paint.valueOf(circleColor));
+        setStyle(style + circleColor);
     }
 
     public void setTooltip(String text) {
