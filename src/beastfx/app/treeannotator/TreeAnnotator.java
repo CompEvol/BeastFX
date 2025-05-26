240e7810cb68948f13014845643799c18d8d9934
Lars-B <berlinglars96@gmail.com>
2025-05-26 09:01:44 -0700

    commenting never used bits of code

--- src/beastfx/app/treeannotator/TreeAnnotator.java
+++ src/beastfx/app/treeannotator/TreeAnnotator.java
@@ -770,52 +770,52 @@ public class TreeAnnotator extends beast.base.inference.Runnable {
         }
     }
 
-    private Tree summarizeTrees(CladeSystem cladeSystem, boolean useSumCladeCredibility) throws IOException  {
+//    private Tree summarizeTrees(CladeSystem cladeSystem, boolean useSumCladeCredibility) throws IOException  {
+//
+//        Tree bestTree = null;
+//        double bestScore = Double.NEGATIVE_INFINITY;
+//
+//        progressStream.println("Analyzing " + totalTreesUsed + " trees...");
+//        progressStream.println("0              25             50             75            100");
+//        progressStream.println("|--------------|--------------|--------------|--------------|");
+//
+//        int stepSize = Math.max(totalTreesUsed / 60, 1);
+//        int reported = 0;
+//
+//        int counter = 0;
+//        treeSet.reset();
+//        while (treeSet.hasNext()) {
+//        	Tree tree = treeSet.next();
+//            double score = scoreTree(tree, cladeSystem, useSumCladeCredibility);
+//          if (score > bestScore) {
+//              bestTree = tree;
+//              bestScore = score;
+//          }
+//		  while (reported < 61 && 1000.0*reported < 61000.0 * (counter + 1) / totalTreesUsed) {
+//              progressStream.print("*");
+//              reported++;
+//              progressStream.flush();
+//    	  }
+//          counter++;
+//        }
+//        progressStream.println();
+//        progressStream.println();
+//        if (useSumCladeCredibility) {
+//            progressStream.println("Highest Sum Clade Credibility: " + bestScore);
+//        } else {
+//            progressStream.println("Highest Log Clade Credibility: " + bestScore);
+//        }
+//
+//        return bestTree;
+//    }
 
-        Tree bestTree = null;
-        double bestScore = Double.NEGATIVE_INFINITY;
-
-        progressStream.println("Analyzing " + totalTreesUsed + " trees...");
-        progressStream.println("0              25             50             75            100");
-        progressStream.println("|--------------|--------------|--------------|--------------|");
-
-        int stepSize = Math.max(totalTreesUsed / 60, 1);
-        int reported = 0;
-
-        int counter = 0;
-        treeSet.reset();
-        while (treeSet.hasNext()) {
-        	Tree tree = treeSet.next();
-            double score = scoreTree(tree, cladeSystem, useSumCladeCredibility);
-          if (score > bestScore) {
-              bestTree = tree;
-              bestScore = score;
-          }
-		  while (reported < 61 && 1000.0*reported < 61000.0 * (counter + 1) / totalTreesUsed) {
-              progressStream.print("*");
-              reported++;
-              progressStream.flush();
-    	  }
-          counter++;
-        }
-        progressStream.println();
-        progressStream.println();
-        if (useSumCladeCredibility) {
-            progressStream.println("Highest Sum Clade Credibility: " + bestScore);
-        } else {
-            progressStream.println("Highest Log Clade Credibility: " + bestScore);
-        }
-
-        return bestTree;
-    }
-
-    public double scoreTree(Tree tree, CladeSystem cladeSystem, boolean useSumCladeCredibility) {
-        if (useSumCladeCredibility) {
-            return cladeSystem.getSumCladeCredibility(tree.getRoot(), null);
-        } else {
-            return cladeSystem.getLogCladeCredibility(tree.getRoot(), null);
-        }
-    }
+//    public double scoreTree(Tree tree, CladeSystem cladeSystem, boolean useSumCladeCredibility) {
+//        if (useSumCladeCredibility) {
+//            return cladeSystem.getSumCladeCredibility(tree.getRoot(), null);
+//        } else {
+//            return cladeSystem.getLogCladeCredibility(tree.getRoot(), null);
+//        }
+//    }
 
 
     private void annotateTree(CladeSystem cladeSystem, Node node, BitSet bits) { //, HeightsSummary heightsOption) {
@@ -1569,100 +1569,100 @@ public class TreeAnnotator extends beast.base.inference.Runnable {
         boolean handleAttribute(Node node, String attributeName, double[] values);
     }
 
-    boolean setTreeHeightsByCA(Tree targetTree, Target targetOption) throws IOException
-             {
-        progressStream.println("Setting node heights...");
-        progressStream.println("0              25             50             75            100");
-        progressStream.println("|--------------|--------------|--------------|--------------|");
-
-        int reportStepSize = totalTreesUsed / 60;
-        if (reportStepSize < 1) reportStepSize = 1;
-        int reported = 0;
-
-
-        // this call increments the clade counts and it shouldn't
-        // this is remedied with removeClades call after while loop below
-        CladeSystem cladeSystem = new CladeSystem(targetTree);
-        final int clades = cladeSystem.getCladeMap().size();
-
-        // allocate posterior tree nodes order once
-        int[] postOrderList = new int[clades];
-        BitSet[] ctarget = new BitSet[clades];
-        BitSet[] ctree = new BitSet[clades];
-
-        for (int k = 0; k < clades; ++k) {
-            ctarget[k] = new BitSet();
-            ctree[k] = new BitSet();
-        }
-
-        cladeSystem.getTreeCladeCodes(targetTree, ctarget);
-
-        // temp collecting heights inside loop allocated once
-        double[][] hs = new double[clades][treeSet.totalTrees - treeSet.burninCount];
-
-        // heights total sum from posterior trees
-        double[] ths = new double[clades];
-
-        int totalTreesUsed = 0;
-
-        int counter = 0;
-        treeSet.reset();
-        while (treeSet.hasNext()) {
-        	Tree tree = treeSet.next();
-            TreeUtils.preOrderTraversalList(tree, postOrderList);
-            cladeSystem.getTreeCladeCodes(tree, ctree);
-            for (int k = 0; k < clades; ++k) {
-                int j = postOrderList[k];
-                for (int i = 0; i < clades; ++i) {
-                    if( CollectionUtils.isSubSet(ctarget[i], ctree[j]) ) {
-                        hs[i][counter] = tree.getNode(j).getHeight();
-                    }
-                }
-            }
-            for (int k = 0; k < clades; ++k) {
-                ths[k] += hs[k][counter];
-            }
-            totalTreesUsed += 1;
-			while (reported < 61 && 1000.0*reported < 61000.0 * (counter + 1) / this.totalTreesUsed) {
-		          progressStream.print("*");
-		          reported++;
-		          progressStream.flush();
-			}
-            counter++;
-
-        }
-
-        if (targetOption != Target.USER_TARGET_TREE)
-            targetTree.initAndValidate();
-
-        cladeSystem.removeClades(targetTree.getRoot(), true);
-        for (int k = 0; k < clades; ++k) {
-            ths[k] /= totalTreesUsed;
-            final Node node = targetTree.getNode(k);
-            node.setHeight(ths[k]);
-            String attributeName = "CAheight";
-            double [] values = hs[k];
-            double min = values[0];
-            double max = values[0];
-            for (double d : values) {
-            	min = Math.min(d, min);
-            	max = Math.max(d, max);
-            }
-            if (Math.abs(min - max) > 1e-10) {
-	            annotateMeanAttribute(node, attributeName + "_mean", values);
-	            annotateMedianAttribute(node, attributeName + "_median", values);
-	            annotateHPDAttribute(node, attributeName + "_95%_HPD", 0.95, values);
-	            annotateRangeAttribute(node, attributeName + "_range", values);
-            }
-        }
-
-        assert (totalTreesUsed == this.totalTreesUsed);
-        this.totalTreesUsed = totalTreesUsed;
-        progressStream.println();
-        progressStream.println();
-
-        return true;
-    }
+//    boolean setTreeHeightsByCA(Tree targetTree, Target targetOption) throws IOException
+//             {
+//        progressStream.println("Setting node heights...");
+//        progressStream.println("0              25             50             75            100");
+//        progressStream.println("|--------------|--------------|--------------|--------------|");
+//
+//        int reportStepSize = totalTreesUsed / 60;
+//        if (reportStepSize < 1) reportStepSize = 1;
+//        int reported = 0;
+//
+//
+//        // this call increments the clade counts and it shouldn't
+//        // this is remedied with removeClades call after while loop below
+//        CladeSystem cladeSystem = new CladeSystem(targetTree);
+//        final int clades = cladeSystem.getCladeMap().size();
+//
+//        // allocate posterior tree nodes order once
+//        int[] postOrderList = new int[clades];
+//        BitSet[] ctarget = new BitSet[clades];
+//        BitSet[] ctree = new BitSet[clades];
+//
+//        for (int k = 0; k < clades; ++k) {
+//            ctarget[k] = new BitSet();
+//            ctree[k] = new BitSet();
+//        }
+//
+//        cladeSystem.getTreeCladeCodes(targetTree, ctarget);
+//
+//        // temp collecting heights inside loop allocated once
+//        double[][] hs = new double[clades][treeSet.totalTrees - treeSet.burninCount];
+//
+//        // heights total sum from posterior trees
+//        double[] ths = new double[clades];
+//
+//        int totalTreesUsed = 0;
+//
+//        int counter = 0;
+//        treeSet.reset();
+//        while (treeSet.hasNext()) {
+//        	Tree tree = treeSet.next();
+//            TreeUtils.preOrderTraversalList(tree, postOrderList);
+//            cladeSystem.getTreeCladeCodes(tree, ctree);
+//            for (int k = 0; k < clades; ++k) {
+//                int j = postOrderList[k];
+//                for (int i = 0; i < clades; ++i) {
+//                    if( CollectionUtils.isSubSet(ctarget[i], ctree[j]) ) {
+//                        hs[i][counter] = tree.getNode(j).getHeight();
+//                    }
+//                }
+//            }
+//            for (int k = 0; k < clades; ++k) {
+//                ths[k] += hs[k][counter];
+//            }
+//            totalTreesUsed += 1;
+//			while (reported < 61 && 1000.0*reported < 61000.0 * (counter + 1) / this.totalTreesUsed) {
+//		          progressStream.print("*");
+//		          reported++;
+//		          progressStream.flush();
+//			}
+//            counter++;
+//
+//        }
+//
+//        if (targetOption != Target.USER_TARGET_TREE)
+//            targetTree.initAndValidate();
+//
+//        cladeSystem.removeClades(targetTree.getRoot(), true);
+//        for (int k = 0; k < clades; ++k) {
+//            ths[k] /= totalTreesUsed;
+//            final Node node = targetTree.getNode(k);
+//            node.setHeight(ths[k]);
+//            String attributeName = "CAheight";
+//            double [] values = hs[k];
+//            double min = values[0];
+//            double max = values[0];
+//            for (double d : values) {
+//            	min = Math.min(d, min);
+//            	max = Math.max(d, max);
+//            }
+//            if (Math.abs(min - max) > 1e-10) {
+//	            annotateMeanAttribute(node, attributeName + "_mean", values);
+//	            annotateMedianAttribute(node, attributeName + "_median", values);
+//	            annotateHPDAttribute(node, attributeName + "_95%_HPD", 0.95, values);
+//	            annotateRangeAttribute(node, attributeName + "_range", values);
+//            }
+//        }
+//
+//        assert (totalTreesUsed == this.totalTreesUsed);
+//        this.totalTreesUsed = totalTreesUsed;
+//        progressStream.println();
+//        progressStream.println();
+//
+//        return true;
+//    }
 
     private CladeSystem cladeSystem = null;
 
